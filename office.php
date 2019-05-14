6a6a19d2127b04663b7924a8e3244390192e143a
tueseed <tue_seed@hotmail.com>
2019-05-14 20:22:49 +0700

    dadsd

---
+++ office.php
@@ -0,0 +1,87 @@
+<div class="row">
+
+    <a class="nav-link col-lg-3 mb-2" href="?action=hub&hub_code=J">
+        <div class="card h-100">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col mr-2">
+                        <h5>งานก่อสร้างค้างนานเกิน 180 วันและเบิกค่าใช้จ่ายเกิน 80 %</h5>
+                        <h6>จำนวน 0 งาน</h6>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-smile fa-4x" style="color:#CC0000"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="overlay">Click</div>
+        </div>
+    </a>
+    <a class="nav-link col-lg-3 mb-2" href="?action=hub&hub_code=J">
+        <div class="card h-100">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col mr-2">
+                        <h5>งานก่อสร้างค้างนานไม่เกิน 180 วันและเบิกค่าใช้จ่ายเกิน 80 %</h5>
+                        <h6>จำนวน 0 งาน</h6>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-smile fa-4x" style="color:#FF8800"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="overlay">Click</div>
+        </div>
+    </a>
+
+    <a class="nav-link col-lg-3 mb-2" href="?action=hub&hub_code=J">
+        <div class="card h-100">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col mr-2">
+                        <h5>งานก่อสร้างค้างนานเกิน 180 วันและเบิกค่าใช้จ่ายไม่เกิน 80 %</h5>
+                        <h6>จำนวน 0 งาน</h6>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-smile fa-4x" style="color:#007E33"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="overlay">Click</div>
+        </div>
+    </a>
+
+    <a class="nav-link col-lg-3 mb-2" href="?action=hub&hub_code=J">
+        <div class="card h-100">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col mr-2">
+                        <h5>งานก่อสร้างค้างนานไม่เกิน 180 วันและเบิกค่าใช้จ่ายไม่เกิน 80 %</h5>
+                        <h6>จำนวน 0 งาน</h6>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-smile fa-4x" style="color:#0099CC"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="overlay">Click</div>
+        </div>
+    </a>
+
+</div>
+<div class="row">
+    <div class="col-lg-12 mb-2" >
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <canvas id="myBarChart"></canvas>
+                    </div>
+                    <div class="col-lg-6">
+                        <canvas id="myPieChart"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
