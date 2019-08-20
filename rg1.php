1a4387934026def8d3740f02fa24e14387016571
tueseed <tue_seed@hotmail.com>
2019-08-20 20:30:40 +0700

    asdasd

--- rg1.php
+++ rg1.php
@@ -13,7 +13,7 @@
       data-page-size="5">
       <thead>
         <tr> 
-        <th data-field="ba" data-sortable="true" data-formatter="textCenterFormatter"><i class="fas fa-bolt"></i> ba</th>
+        <th data-field="ba" data-sortable="true" data-formatter="textCenterFormatter"><i class="fas fa-bolt"></i> รหัสการไฟฟ้า</th>
         <th data-field="wbs" data-sortable="true" data-formatter="textCenterFormatter"><i class="fas fa-bolt"></i> WBS</th>
         <th data-field="description" data-sortable="true" data-formatter="textCenterFormatter"><i class="fas fa-bolt"></i> คำอธิบาย</th>
         <th data-field="office_name" data-sortable="true" data-formatter="textCenterFormatter"><i class="fas fa-bolt"></i> หน่วยงาน</th>
@@ -30,8 +30,8 @@
     </table>
 </div> 
 <div class="card-footer">
-<div class="float-left">
-        <div class="dropup d-inline-block">
+  <div class="float-left">
+        <!-- <div class="dropup d-inline-block">
           <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             คอลัมน์
           </button>
@@ -39,7 +39,47 @@
             <label class="dropdown-item" ><input type="checkbox" id="wbs" value="wbs" onclick="col(this.value)" checked/>&nbsp;WBS</label>
             <a class="dropdown-item" href="#" ><input id="description" type="checkbox" value="description" onclick="col(this.value)" checked/>&nbsp;รายละเอียด</a>
           </div>
-        </div>
-        </div>
+        </div> -->
+        <button class="btn btn-sm btn-outline-primary" href="#" title="Like" data-toggle="modal"  data-target="#column_modal">
+          คอลัมน์
+        </button>
+  </div>
 </div>  
+</div>
+
+<div class='modal fade' tabindex='-1' role='dialog' id='column_modal'>
+  <div class='modal-dialog modal-lg' role='document' >
+    <div class='modal-content'>
+      <div class='modal-header'>
+        <h5 class='modal-title font-weight-bold' id="head_modal">เลือกกการแสดงคอลัมน์ <span id="pea_no"></span></h5>
+        <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+          <span aria-hidden='true'>&times;</span>
+        </button>
+      </div>
+      <div class='modal-body'>
+        <div class="row">
+          <div class="col-lg-4">
+            <label class="dropdown-item" ><input type="checkbox" id="ba" value="ba" onclick="col(this.value)" checked/>&nbsp;รหัสการไฟฟ้า</label>
+            <label class="dropdown-item" ><input type="checkbox" id="wbs" value="wbs" onclick="col(this.value)" checked/>&nbsp;WBS</label> 
+            <label class="dropdown-item" ><input type="checkbox" id="description" value="description" onclick="col(this.value)" checked/>&nbsp;คำอธิบาย</label> 
+            <label class="dropdown-item" ><input type="checkbox" id="office_name" value="office_name" onclick="col(this.value)" checked/>&nbsp;หน่วยงาน</label>
+          </div>
+          <div class="col-lg-4">
+            <label class="dropdown-item" ><input type="checkbox" id="user_status" value="user_status" onclick="col(this.value)" checked/>&nbsp;สถานะผู้ใช้</label> 
+            <label class="dropdown-item" ><input type="checkbox" id="network" value="network" onclick="col(this.value)" checked/>&nbsp;เลขที่โครงข่าย</label>
+            <label class="dropdown-item" ><input type="checkbox" id="postg_date" value="postg_date" onclick="col(this.value)" checked/>&nbsp;วันที่เบิกของครั้งแรก</label> 
+            <label class="dropdown-item" ><input type="checkbox" id="day_now" value="day_now" onclick="col(this.value)" checked/>&nbsp;จำนวนวัน</label> 
+          </div>
+          <div class="col-lg-4">
+            <label class="dropdown-item" ><input type="checkbox" id="percent" value="percent" onclick="col(this.value)" checked/>&nbsp;ค่าแรง(%)</label> 
+            <label class="dropdown-item" ><input type="checkbox" id="tech_name" value="tech_name" onclick="col(this.value)" checked/>&nbsp;ชื่อ-สกุล</label>
+            <label class="dropdown-item" ><input type="checkbox" id="tech_id" value="tech_id" onclick="col(this.value)" checked/>&nbsp;รหัสพนักงาน</label> 
+            <label class="dropdown-item" ><input type="checkbox" id="act" value="act" onclick="col(this.value)" checked/>&nbsp;ค่าใช้จ่ายจริง</label> 
+          </div>
+                  
+        </div> 
+      </div>
+                        
+    </div>
+  </div>
 </div>
