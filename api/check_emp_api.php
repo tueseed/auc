a411f44a82a40c5ffdb2380a0d9567acb0d0ac78
tueseed <tue_seed@hotmail.com>
2019-08-26 00:03:07 +0700

    sdadasd

--- api/check_emp_api.php
+++ api/check_emp_api.php
@@ -1,8 +1,8 @@
 <?php
 require('../utils/connect_emp.php');
 $emp_id = $_POST["emp_id"];
-
-$sql = "SELECT DEPT_SHORT FROM CMNDB_EMPLOYEE_INFO WHERE EMPLOYEE_ID LIKE '__$emp_id'"; 
+$emp_sub = substr($emp_id,-6);
+$sql = "SELECT DEPT_SHORT FROM CMNDB_EMPLOYEE_INFO WHERE EMPLOYEE_ID LIKE '__$emp_sub'"; 
 $stmt = sqlsrv_query( $conn, $sql);
 $obj = sqlsrv_fetch_array($stmt,SQLSRV_FETCH_ASSOC);
 echo json_encode($obj);
